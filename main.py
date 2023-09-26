3f8a5f058ddf44d783dc82b633cf557b5d5a8d0a
eurwlafl <144701073+eurwlafl@users.noreply.github.com>
2023-09-26 12:02:23 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# crée par Willem Lafleur en 2023
+
+#La fonction
+def count_word():
+#La phrase où les mots vont êtres countés
+    word=("Une belle phrase")
+#compte le nombre d`espaces et ajoute 1 pour avoir le nombre de mots
+    wordcount=(word.count(" ")+1)
+#montre le nombre de mots
+    print(wordcount)
+#appel la fonction
+count_word()
